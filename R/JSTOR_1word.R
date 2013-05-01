62dfa82a3501627c5389aa017ad1e6b2c358d9e0
Ben Marwick <benmarwick@hotmail.com>
2013-05-01 12:15:53 -0700

    added span variable

--- R/JSTOR_1word.R
+++ R/JSTOR_1word.R
@@ -3,14 +3,15 @@
 #' @description Function to plot changes in the relative frequency of a word over time. The relative frequency is the frequency of the word in a document divided by the total number of words in a document. For use with JSTOR's Data for Research datasets (http://dfr.jstor.org/).
 #' @param x object returned by the function JSTOR_unpack.
 #' @param oneword One word, surrounded by standard quote marks, or a vector of words
+#' @param span span of the lowess line (controls the degree of smoothing)
 #' @return Returns a ggplot object with publication year on the horizontal axis and log relative frequency on the vertical axis. Each point represents a single document.
 #' @examples 
-#' ##JSTOR_1word(unpack, "diamonds")
-#' ##JSTOR_1word(unpack, c("diamonds", pearls"))
+#' ##JSTOR_1word(unpack, "diamonds", span = 0.4)
+#' ##JSTOR_1word(unpack, c("diamonds", pearls"), span = 0.4)
 
 
 
-JSTOR_1word <- function(x, oneword){
+JSTOR_1word <- function(x, oneword, span){
   #### investigate change in use of certain words of interest over time
   wordcounts <- x$wordcounts
   bibliodata <- x$bibliodata
@@ -28,7 +29,7 @@ JSTOR_1word <- function(x, oneword){
   library(ggplot2)
   suppressWarnings(ggplot(word_by_year, aes(year, log(word_ratio))) +
                      geom_point(subset = .(word_ratio > 0)) +
-                     geom_smooth( aes(group=1), method = "loess", span = 0.4, data=subset(word_by_year, word_ratio>0)) +
+                     geom_smooth( aes(group=1), method = "loess", span = span, data=subset(word_by_year, word_ratio>0)) +
                      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
                      ylab(paste0("log of frequency of the word '", oneword, "'")) +
                      # inspect bibliodata$year to see min and max year to set axis limits
