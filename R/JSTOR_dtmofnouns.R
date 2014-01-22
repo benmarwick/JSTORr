1fd8f92ded38d5a53122e181e632f643c5b67ca7
Ben Marwick <benmarwick@gmail.com>
2014-01-22 11:45:54 -0800

    move tagPOS function

--- R/JSTOR_dtmofnouns.R
+++ R/JSTOR_dtmofnouns.R
@@ -127,6 +127,19 @@ if(parallel) {
   
   
  } else { # non-parallel method
+   
+   tagPOS <-  function(x) {
+     s <- as.String(x)
+     word_token_annotator <- Maxent_Word_Token_Annotator()
+     PTA <- Maxent_POS_Tag_Annotator()
+     a2 <- Annotation(1L, "sentence", 1L, nchar(s))
+     a2 <- annotate(s, word_token_annotator, a2)
+     a3 <- annotate(s, PTA, a2)
+     a3w <- a3[a3$type == "word"]
+     POStags <- unlist(lapply(a3w$features, `[[`, "POS"))
+     POStagged <- paste(sprintf("%s/%s", s[a3w], POStags), collapse = " ")
+     list(POStagged = POStagged, POStags = POStags)
+   }
 
   pos <- tagPOS(y$dimnames$Terms)
 
