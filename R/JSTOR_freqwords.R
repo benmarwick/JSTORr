3dc94559fef4d653a8837424ad0bc9ee1f85a63a
rstudio <rstudio@example.com>
2015-02-24 23:02:26 +0000

    added unpack multiple archives

--- R/JSTOR_freqwords.R
+++ R/JSTOR_freqwords.R
@@ -13,83 +13,95 @@
 #' ## freqwords <- JSTOR_freqwords(unpack1grams, nouns)
 
 
-JSTOR_freqwords <- function(unpack1grams, nouns, n=5, lowfreq=300, topn = 20, biggest = 10){
+
+JSTOR_freqwords <- function (unpack1grams, nouns, custom_stopwords = NULL, n = 5, lowfreq = 300, topn = 20, 
+                             biggest = 10 ) 
+{
   
-  # get bibliodata ready
+  # bibliodata <- unpack_multi$bibliodata
   bibliodata <- unpack1grams$bibliodata
   message("removing stopwords...")
-  y <- nouns[, !(nouns$dimnames$Terms %in% stopwords(kind = "en")) ]
+  y <- nouns[, !(nouns$dimnames$Terms %in% c(custom_stopwords, stopwords(kind = "en"))) ]
+  
+  
+  # make sure we have a good match between DOIs and years
+  DOIs_in_dtm <- data.frame(DOI = y$dimnames$Docs)
+  DOIs_and_years_in_bibliodata <- data.frame(DOI = bibliodata$x,
+                                             year = as.numeric(substring(bibliodata$issue, 1, 4)))      
+  library(plyr)
+  DOIs_and_years_in_dtm <- join(DOIs_in_dtm, DOIs_and_years_in_bibliodata, by = "DOI")
+  # rename docs with year of publication, need to have Docs as character to subset it...
+  y$dimnames$Docs <- as.character(DOIs_and_years_in_dtm$year)
+  
+  # subset bibliodata in case we filtered the nouns by a word
+  
+  bibliodata_subset <- bibliodata[(bibliodata$year %in% DOIs_and_years_in_dtm$year), ]
+  
+  
   message("done")
   require(plyr)
-  
-  bibliodata$year <- as.numeric(as.character(bibliodata$year))
-  uniqueyears <- sort(unique(bibliodata$year))
+  bibliodata_subset$year <- as.numeric(as.character(bibliodata_subset$year))
+  uniqueyears <- sort(unique(bibliodata_subset$year))
   
   message("getting information about the documents...")
-  # put names on the Docs that we can use to get year info from
-  titlefrag <- llply(1:nrow(bibliodata), function(i) paste(unlist(strsplit(as.character(gsub('[[:punct:]]','', bibliodata$doi)[i]), " "))[1:5], collapse = "."), .progress = "text", .inform = FALSE)
-  y$dimnames$Docs <- paste0(bibliodata$year, "-", titlefrag)
-  message("done")
+  titlefrag <- llply(1:nrow(bibliodata_subset), function(i) paste(unlist(strsplit(as.character(gsub("[[:punct:]]", 
+                                                                                                    "", bibliodata_subset$doi)[i]), " "))[1:5], collapse = "."), 
+                     .progress = "text", .inform = FALSE)
   
-  # split the years vector into chunks that are n years big
+  message("done")
   x <- seq_along(uniqueyears)
-  # get list of chunks, each chunk has n years in it
   years <- split(uniqueyears, ceiling(x/n))
-  # create list of dtms by subsetting the full dtm into seperate
-  # dtms of documents where publication year matches the chunks
-  # of years
-  message(paste0("splitting the document term matrix into chunks of ",n , " years..."))
-  dtm1 <- vector(mode = "list", length = length(years) )
-  dtmlist <- llply(1:length(years), function(i) dtm1[[i]] <- y[as.numeric(substring(y$dimnames$Docs, 1, 4)) %in% unname(unlist(years[i])),], .progress = "text", .inform = FALSE)
-  message("done")
+  message(paste0("splitting the document term matrix into chunks of ", 
+                 n, " years..."))
   
+  
+  
+  dtm1 <- vector(mode = "list", length = length(years))
+  dtmlist <- llply(1:length(years), function(i) 
+    dtm1[[i]] <- y[(y$dimnames$Docs 
+                    %in% unname(unlist(years[i]))), ], 
+    .progress = "text",  .inform = FALSE)
+  
+  
+  
+  message("done")
   message("building x-axis labels...")
-  # put names on the list of dtm so we can see the years represented by each dtm
-  names(dtmlist) <- llply(years, function(i) paste0(min(i),"-",max(i) ))
+  names(dtmlist) <- llply(years, function(i) paste0(min(i), 
+                                                    "-", max(i)))
   message("done")
-  
-  message(paste0("calculating most frequent words in each ", n, " year chunk..."))
-  # Here's the heavy lifting...
-  # find most frequent words per chunk (results are in alpha order, not very useful)
-  freqterms <- llply(dtmlist, function(i) findFreqTerms(i, lowfreq = lowfreq, highfreq = Inf), .progress = "text", .inform = FALSE)
-  
+  message(paste0("calculating most frequent words in each ", 
+                 n, " year chunk..."))
+  freqterms <- llply(dtmlist, function(i) findFreqTerms(i, 
+                                                        lowfreq = lowfreq, highfreq = Inf), .progress = "text", 
+                     .inform = FALSE)
   message("done")
-  
   message("putting the words in rank order...")
-  # get frequencies of words to put in order of abundance, rather than alpha
-  freqterms1 <- llply(1:length(years), function(i) colSums(as.matrix(dtmlist[[i]])[, freqterms[[i]]]), .progress = "text", .inform = FALSE)
-  # reshape into dataframes and sort with most freq word at the top
+  freqterms1 <- llply(1:length(years), 
+                      function(i) colSums(as.matrix(dtmlist[[i]])[,  freqterms[[i]], drop=FALSE]), 
+                      .progress = "text", .inform = FALSE)
+  
+  
   require(plyr)
-  freqterms2 <- llply(1:length(years), function(i) arrange(data.frame(word = names(freqterms1[[i]]), count = unname(freqterms1[[i]])), desc(count)), .progress = "text", .inform = FALSE)
+  freqterms2 <- llply(1:length(years), function(i) arrange(data.frame(word = names(freqterms1[[i]]), 
+                                                                      count = unname(freqterms1[[i]])), desc(count)), .progress = "text", 
+                      .inform = FALSE)
   names(freqterms2) <- names(dtmlist)
-  # combine list of dataframes into one big dataframe with word, freq and year-range
   freqterms3 <- do.call(rbind, freqterms2)
-  # make a year-range col by getting everything before the period in the rowname
-  freqterms3$year <- gsub("\\..*","", rownames(freqterms3))
-  freqterms3$rank <- as.numeric(gsub(".*\\.","", rownames(freqterms3)))
-  message('done')
-  
-  # plot in a word-cloud-y kind of way, but with more useful information in the 
-  # structure of the visualiation... 
+  freqterms3$year <- gsub("\\..*", "", rownames(freqterms3))
+  freqterms3$rank <- as.numeric(gsub(".*\\.", "", rownames(freqterms3)))
+  message("done")
   require(ggplot2)
-  suppressWarnings(print(ggplot(freqterms3, aes(factor(year), rank)) + 
-                           geom_text(aes(label = word, size = count, alpha = count), data = subset(freqterms3, rank < topn)) +
-                           scale_y_reverse() + 
-                           scale_size(range = c(3, biggest), name = "Word count") +
-                           scale_alpha(range=c(0.5,1), limits=c(min(freqterms3$count),max(freqterms3$count)), guide = 'none') + 
-                           xlab("Year range") +
-                           ylab("Rank order of word") +
-                           # coord_fixed(ratio = (1/(1+sqrt(5))/2)  ) +
-                           theme(
-                             panel.background=element_blank(),
-                             panel.border=element_blank(),
-                             panel.grid.major=element_blank(),
-                             panel.grid.minor=element_blank(),
-                             plot.background=element_blank(),
-                             axis.text.x=element_text(angle = 90, hjust = 0))
-  ))
-  
-  # return the dataframe in case anything else is wanted with it
+  suppressWarnings(print(ggplot(freqterms3, aes(factor(year), 
+                                                rank)) + geom_text(aes(label = word, size = count, alpha = count), 
+                                                                   data = subset(freqterms3, rank < topn)) + scale_y_reverse() + 
+                           scale_size(range = c(3, biggest), name = "Word count") + 
+                           scale_alpha(range = c(0.5, 1), limits = c(min(freqterms3$count), 
+                                                                     max(freqterms3$count)), guide = "none") + xlab("Year range") + 
+                           ylab("Rank order of word") + theme(panel.background = element_blank(), 
+                                                              panel.border = element_blank(), panel.grid.major = element_blank(), 
+                                                              panel.grid.minor = element_blank(), plot.background = element_blank(), 
+                                                              axis.text.x = element_text(angle = 90, hjust = 0))))
   return(freqterms3)
-  
 }
+
+
---
+++ R/JSTOR_unpack_multiple_archives.R
@@ -0,0 +1,165 @@
+#' Plot the most frequent words by time intervals
+#' 
+#' @description Generates a plot of the top n words in all the documents in ranges of years. For use with JSTOR's Data for Research datasets (http://dfr.jstor.org/). For best results, repeat the function several times after adding common words to the stopword list and excluding them by re-running the JSTOR_dtmofnouns function. The location of the English stopwords list can be found by entering this at the R prompt: paste0(.libPaths()[1], "/tm/stopwords/english.dat")
+#' @param mydir path to directory containing multiple zip files dowloaded from dfr.jstor.org (default is the current working directory)
+#' @return Returns a list of two items. First is "wordcounts", a Document Term Matrix of 1-grams, and second is 'bibliodata', a data frame of bibliographic information for all articles. 
+
+#' @examples 
+#' ## multiple_archives <- JSTOR_unpack_multiple_archives(mydir = "~/my_data")
+
+
+
+JSTOR_unpack_multiple_archives <- function(mydir = getwd()){
+
+### combine multiple JSTOR DFR archives
+
+# assume we have a folder of several zip files that are
+# JSTOR DFR archives
+
+# get file names
+
+zips <- list.files(mydir, full.names = TRUE, pattern = "zip")
+dirnames <- gsub(".zip", "", basename(zips))
+
+# loop over the zip files and unzip into their own folder
+# (otherwise they'll overwrite each other)
+for(i in 1:length(zips)){
+  unzip(zips[i], exdir = paste0(mydir, "/", dirnames[i]))
+}
+
+# loop over all the 'wordcounts' csv files and combine into
+# one mega data table
+unzips <- list.files(mydir, full.names = TRUE, recursive = TRUE)
+all_wordcounts <- unzips[grep("wordcounts", unzips)]
+
+# library(data.table)
+# all_wordcount_csvs_list <- lapply(all_wordcounts,  fread)
+# # add DOI
+# DOIs <- substr(all_wordcounts, (nchar(all_wordcounts)-19), (nchar(all_wordcounts)-4))
+# names(all_wordcount_csvs_list) <- DOIs
+# # make into one giant table
+# all_wordcount_csvs_dt <- rbindlist(all_wordcount_csvs_list)
+# all_wordcount_csvs_dt[, "names" := rep(DOIs, sapply(all_wordcount_csvs_list, function(x) length(x[[1]]), USE.NAMES=FALSE))]
+# 
+# loop over citation files...
+all_citations <- unzips[grep("citation", unzips)]
+all_citations_csvs <- lapply(all_citations,  read.csv, quote="", row.names=NULL, comment.char = "", header = TRUE,  stringsAsFactors = FALSE, colClasses="character")
+library("data.table")
+all_citations_csvs <- rbindlist(all_citations_csvs)
+
+###############
+
+library(plyr); library(data.table)
+read_csv2dt <- function(x) data.table(fread(x, sep = ",", stringsAsFactors=FALSE))
+aawc <-  llply(all_wordcounts, read_csv2dt, .progress = "text", .inform = FALSE)
+# give the DOIs
+names(aawc) <- all_wordcounts
+# Identify empty CSV files and exclude them
+lens <- sapply(aawc, function(i) i[1]$WEIGHT + i[2]$WEIGHT + i[3]$WEIGHT)
+full <- unname(!is.na(lens))
+# Subset only CSV files with at least three words...
+aawc1 <- aawc[full]
+
+# custom version of tm::DocumentTermMatrix for 1-grams
+library(slam); library(tm)
+my_dtm_1gram <- function(x){ 
+  y <- as.integer(x$WEIGHT)
+  names(y) <- x$WORDCOUNTS
+  v =  unname(y)          # num
+  i = rep(1, length(y))   # int
+  j = seq(1:length(y))    # int
+  z <- simple_triplet_matrix(v = v,   # num
+                             i = i,        # int
+                             j = j,        # int
+                             nrow = max(i),
+                             ncol = max(j), 
+                             dimnames =                          
+                               list(                              
+                                 Docs = deparse(substitute(x)),
+                                 Terms = names(y)))
+  zz <- as.DocumentTermMatrix(z, weighting = weightTf)
+  return(zz)
+}
+# use
+aawc2 <- llply(1:length(aawc1), function(i) my_dtm_1gram(aawc1[[i]]), .progress = "text", .inform = FALSE)
+
+
+# assign file names to each dataframe in the list
+myfiles1 <- all_wordcounts[full]
+library(stringr)
+names(aawc2) <- str_extract(basename(myfiles1), "[^wordcounts_].+[^.CSV]")
+
+#Now work with biblio data
+# replace for-slash with underscore to make it match the filenames
+# and replace odd \t that was added during import 
+library(stringr)
+all_citations_csvs$id <- str_extract(chartr('/', '_', all_citations_csvs$id), ".*[^\t]")
+
+# limit list of citations to full length articles only 
+# note that citation type is not in the correct column
+# and that we need \t in there also
+# changed this in case we get a dataset that was not originally all fla
+all_citations_fla <- unique(all_citations_csvs[all_citations_csvs$publisher == 'fla\t',])
+# subset from the wordcount data only the full length articles
+
+# subset items in the list of wordcount data whose names are in 
+# the list of fla citation IDs
+wordcounts_fla <- aawc2[which(names(aawc2) %in%  all_citations_fla$id)]
+
+# drop citations that we dont' have text for
+all_citations_fla <- all_citations_fla[which(all_citations_fla$id %in% names(wordcounts_fla))]
+
+# put citation IDs in the same order with wordcount data names
+# which is the same order as myfiles
+bibliodata <- data.table(merge(names(wordcounts_fla), all_citations_fla, by.x=1, by.y="id"))
+# create a variable that holds the year of publication for
+# each article
+bibliodata$year <- str_extract(bibliodata$issue, "[[:digit:]]+{4}")
+
+# remove duplicate articles (where the same DOI occurs more than once)
+
+# clean up and next step is very memory
+keep <- c("bibliodata", "wordcounts_fla")
+rm(list=ls()[! ls() %in% keep])
+
+# combine into one giant dtm, rather slow
+library(plyr)
+wordcounts_combined <- do.call(tm:::c.DocumentTermMatrix, wordcounts_fla)
+
+# give docs their DOI as names (this was wrong! should be good now)
+# wordcounts_combined and wordcounts_fla are in the same order
+# but bibliodata is not
+wordcounts_combined$dimnames$Docs <- names(wordcounts_fla)
+
+# delete large object that's no longer needed
+rm(wordcounts_fla)
+
+# somehow docs got to be a factor... fix this
+# and why do I have to subset the Docs like this?
+# something to do with how the DTM is made? I bet
+# the CSV file is dodgy... 
+wordcounts <- wordcounts_combined[unique(as.character(wordcounts_combined$dimnames$Docs[1:nrow(wordcounts_combined)])), ]
+rm(wordcounts_combined)
+
+# put bibliodata in the same order as wordcounts, cf http://stackoverflow.com/a/11977256/1036500
+bibliodata <- bibliodata[match(wordcounts$dimnames$Docs, bibliodata$x),]
+
+message("removing stopwords...")
+wordcounts <- wordcounts[, !(wordcounts$dimnames$Terms %in% stopwords(kind = "en")) ]
+message("done")
+
+message("discarding words with <3 characters (probably OCR errors)...")
+wordcounts <- wordcounts[,nchar(wordcounts$dimnames$Terms) > 3]
+message("done")
+
+message("discarding words with >2 consecutive characters (probably OCR errors)...")
+wordcounts <- wordcounts[,!grepl("(.)\\1{2,}", wordcounts$dimnames$Terms)]
+message("done")
+
+message("discarding non-ASCII characters...")
+wordcounts <- wordcounts[,(wordcounts$dimnames$Terms %in% iconv(wordcounts$dimnames$Terms, "latin1", "ASCII", sub=""))]
+message("done")
+
+unpack_multi <- list("wordcounts" = wordcounts, "bibliodata" = bibliodata)
+rm(wordcounts, bibliodata)
+}
